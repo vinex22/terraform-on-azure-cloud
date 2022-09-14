b8f6c72e416e3e90e1fcc6ac5a0abc288c87931d
Vinay Jain <vinex22@gmail.com>
2022-09-14 19:22:31 +0800

    v

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
-  location = "eastus"
-  name = "my-demo-rg1"  
+resource "azurerm_resource_group" "vinex2212" {
+  location = "eastasia"
+  name = "vinex2212"  
 }
